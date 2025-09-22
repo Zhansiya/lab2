f85416af81a0f9a5109e781e6e6201ccb4a8b9f6
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-22 13:12:31 +0500

    Example 3

--- operators.py
+++ operators.py
@@ -63,3 +63,27 @@ not in	Returns True if a sequence with the specified value is not present in the
 <<	Zero fill left shift		                                x << 2	
 >>	Signed right shift		                                    x >> 2
 '''
+
+#***************************#
+
+print((6 + 3) - (6 + 3))
+
+print(100 + 5 * 3)
+
+'''Operator Precedence
+()	            Parentheses	
+**	            Exponentiation	
++x  -x  ~x	    Unary plus, unary minus, and bitwise NOT	
+*  /  //  %	    Multiplication, division, floor division, and modulus	
++  -	        Addition and subtraction	
+<<  >>	        Bitwise left and right shifts	
+&	            Bitwise AND	
+^	            Bitwise XOR	
+|	            Bitwise OR	
+==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators	
+not	            Logical NOT	
+and	            AND	
+or	            OR
+'''
+
+print(5 + 4 - 7 + 3)
