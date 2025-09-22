65c7720206d4c3643224b9997f0ef59aece15069
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-22 12:29:39 +0500

    Example 3

--- booleans.py
+++ booleans.py
@@ -2,6 +2,8 @@ print(10 > 9)
 print(10 == 9)
 print(10 < 9)
 
+#***************************#
+
 a = 200
 b = 33
 
@@ -9,3 +11,10 @@ if b > a:
     print("b is greater than a")
 else:
     print("b is not greater than a")
+
+#***************************#
+
+print(bool("Hello!"))
+print(bool(15))
+
+#***************************#
