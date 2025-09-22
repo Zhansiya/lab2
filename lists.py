de8e1834bcbb05241543b490885503dc1126696d
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-22 13:20:11 +0500

    Example 4

--- lists.py
+++ lists.py
@@ -11,3 +11,9 @@ print(thislist)
 print(len(thislist))
 
 #***************************#
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+#***************************#
