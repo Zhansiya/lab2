13a8821d4633c252e8de6c47ea4276bbcafc39c8
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-22 13:17:04 +0500

    Example 2

--- lists.py
+++ lists.py
@@ -2,3 +2,8 @@ thislist = ["apple", "banana", "cherry"]
 print(thislist)
 
 #***************************#
+
+thislist = ["apple", "banana", "cherry", "aplle", "cherry"]
+print(thislist)
+
+#***************************#
