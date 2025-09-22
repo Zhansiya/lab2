6971c15e94d49f2132eed95104b15e84449c706e
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-22 12:37:59 +0500

    Example 6

--- booleans.py
+++ booleans.py
@@ -26,3 +26,27 @@ print(bool(x))
 print(bool(y))
 
 #***************************#
+''''True'''
+bool("abc")
+bool(123)
+bool(["apple", "banana", "cherry"])
+
+'''Fasle'''
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+#***************************#
+
+class myclass():
+    def __len__(self):
+        return 0
+    
+myobj = myclass()
+print(bool(myobj))
+
+#***************************#
