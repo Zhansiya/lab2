8a349238bc5158f6c68c364846576245226cd70e
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 09:13:48 +0500

    Example 12

--- lists.py
+++ lists.py
@@ -57,3 +57,29 @@ if "apple" in thislist:
   print("Yes, 'apple' is in the fruits list")
 
 #***************************#
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
+#***************************#
