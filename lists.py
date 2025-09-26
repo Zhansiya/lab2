32b0d26b054a1ede3eb75b331891b21f926b5e50
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 09:05:03 +0500

    Example 9

--- lists.py
+++ lists.py
@@ -36,3 +36,8 @@ thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 print(thislist[2:5])
 
 #***************************#
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+#***************************#
