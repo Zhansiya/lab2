1b187fa49b6f41d19dcb857924ceb64fd5c95fc6
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 09:16:51 +0500

    Example 41

--- lists.py
+++ lists.py
@@ -106,3 +106,36 @@ thislist.extend(thistuple)
 print(thislist)
 
 #***************************#
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist.remove("banana")
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
