05e47b503089b1f5956b5119fb429d9f37dec855
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 10:07:13 +0500

    Example 6

--- dictionaries.py
+++ dictionaries.py
@@ -230,3 +230,25 @@ thisdict = {
 }
 thisdict.clear()
 print(thisdict)
+
+
+#***************************#
+
+for x in thisdict:
+  print(x)
+
+
+for x in thisdict:
+  print(thisdict[x])
+
+
+for x in thisdict.values():
+  print(x)
+
+
+for x in thisdict.keys():
+  print(x)
+
+
+for x, y in thisdict.items():
+  print(x, y)
