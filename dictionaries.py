29a48a8cc01e9e1b4ec0abab1ea64d1488899826
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 10:02:13 +0500

    Exxample 3

--- dictionaries.py
+++ dictionaries.py
@@ -159,5 +159,21 @@ thisdict = {
 if "model" in thisdict:
   print("Yes, 'model' is one of the keys in the thisdict dictionary")
 
+#***************************#
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["year"] = 2018
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"year": 2020})
 
 
