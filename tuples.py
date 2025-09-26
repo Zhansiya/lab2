7d298ad920a418df726156855818e793bb169ecc
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 09:41:11 +0500

    Example 7

--- tuples.py
+++ tuples.py
@@ -162,4 +162,9 @@ mytuple = fruits * 2
 
 print(mytuple)
 
-#***************************#
+#***************************# 
+
+'''
+count()	Returns the number of times a specified value occurs in a tuple
+index()	Searches the tuple for a specified value and returns the position of where it was found
+'''
