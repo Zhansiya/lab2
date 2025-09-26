0517e1982e10426bd70c23556aa2d1b4749d82aa
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 09:46:14 +0500

    Example 3

--- set.py
+++ set.py
@@ -57,4 +57,26 @@ thisset = {"apple", "banana", "cherry"}
 
 print("banana" not in thisset)
 
+#***************************#
 
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+tropical = {"pineapple", "mango", "papaya"}
+
+thisset.update(tropical)
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
