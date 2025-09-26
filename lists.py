18c925d3721128c3797260b762dcb396981b2546
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 09:24:39 +0500

    Example 18

--- lists.py
+++ lists.py
@@ -246,3 +246,20 @@ thislist.reverse()
 print(thislist)
 
 #***************************#
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
+
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist[:]
+print(mylist)
+
+#***************************#
+
