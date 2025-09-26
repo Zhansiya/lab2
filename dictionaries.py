3af239ec1b24767be5503dd21da2c02499bc164a
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 10:03:56 +0500

    Example 4

--- dictionaries.py
+++ dictionaries.py
@@ -176,4 +176,22 @@ thisdict = {
 }
 thisdict.update({"year": 2020})
 
+#***************************#
 
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(thisdict)
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.update({"color": "red"})
+
+#***************************#
