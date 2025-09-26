d60258ab3c712de42077967d4d158153eaa7ca0d
Zhansiya <z_zheldybay@kbtu.kz>
2025-09-26 09:48:07 +0500

    Example 5

--- set.py
+++ set.py
@@ -80,3 +80,41 @@ mylist = ["kiwi", "orange"]
 thisset.update(mylist)
 
 print(thisset)
+
+#***************************#
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.discard("banana")
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+x = thisset.pop()
+
+print(x)
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+
+del thisset
+
+print(thisset)
